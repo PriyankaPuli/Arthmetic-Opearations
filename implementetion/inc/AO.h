fb0cf5fdcc05bacd54544de9bc7023a69b4e7eea
PriyankaPuli <52269585+PriyankaPuli@users.noreply.github.com>
2020-12-06 15:42:53 +0530

    Update AO.h

--- implementetion/inc/AO.h
+++ implementetion/inc/AO.h
@@ -1,1 +1,13 @@
+#ifndef AO_H_INCLUDED
+#define AO_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int n1, int n2);
+int subtract(int n1, int n2);
+int multiply(int n1, int n2);
+int divide(int n1, int n2);
+
+
+#endif // AO_H_INCLUDED
 
