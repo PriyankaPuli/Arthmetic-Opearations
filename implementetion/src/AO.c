b03daac0194741e512e563bffe038be3eb0dfde5
PriyankaPuli <52269585+PriyankaPuli@users.noreply.github.com>
2020-12-06 15:42:17 +0530

    Update AO.c

--- implementetion/src/AO.c
+++ implementetion/src/AO.c
@@ -1,1 +1,26 @@
+#include <AO.h>
+int add(int n1, int n2)
+{
+  int result;
+  result = n1 + n2;
+  return result;
+}
+int subtract(int n1, int n2)
+{
+  int result;
+  result = n1 - n2;
+  return result;
+}
+int multiply(int n1, int n2)
+{
+  int result;
+  result = n1 * n2;
+  return result;
+}
+int divide(int n1, int n2)
+{
+  int result;
+  result = n1 / n2;
+  return result;
+}
 
