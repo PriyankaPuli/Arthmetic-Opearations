ecd7887aaede840303991aa1b019a510c2416288
PriyankaPuli <52269585+PriyankaPuli@users.noreply.github.com>
2020-12-06 15:48:02 +0530

    Update test.c

--- implementetion/Test/test.c
+++ implementetion/Test/test.c
@@ -1,1 +1,31 @@
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+#include<telecom.h>
+#define PROJECT_NAME    "Telecom_Billing_Management_System"
+
+void test_addrecords(void);
+void test_listrecords(void);
+void test_modifyrecords(void);
+void test_searchrecords(void);
+void test_payment(void);
+
+int main() {
+
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+ 
+  CU_add_test(suite, "addrecords", test_addrecords);
+  CU_add_test(suite, "listrecords", test_listrecords);
+  CU_add_test(suite, "modifyrecords", test_modifyrecords);
+  CU_add_test(suite, "searchrecords", test_searchrecords);
+  CU_add_test(suite, "payment", test_payment);
+ 
+
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  CU_basic_run_tests();
+  CU_cleanup_registry();
+  return 0;
+}
+
 
