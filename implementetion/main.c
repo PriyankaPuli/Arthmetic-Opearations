df1199138f1c14fa340dd34fc4aa74e58618b611
PriyankaPuli <52269585+PriyankaPuli@users.noreply.github.com>
2020-12-06 15:43:36 +0530

    Update main.c

--- implementetion/main.c
+++ implementetion/main.c
@@ -1,1 +1,16 @@
+#include <AO.h>
+#include <stdio.h>
+int main()
+{
+  int num1, num2;
 
+  printf("Enter two numbers: ");
+  scanf("%d %d", &num1, &num2);
+
+  printf("%d + %d = %d\n", num1, num2, add(num1, num2));
+  printf("%d - %d = %d\n", num1, num2, subtract(num1, num2));
+  printf("%d * %d = %d\n", num1, num2, multiply(num1, num2));
+  printf("%d / %d = %d\n", num1, num2, divide(num1, num2));
+
+  return 0;
+}
